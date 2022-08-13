5f1845f8b94265ade7eaa3cd169f85451bbe2eb2
Szabolcs Sipos <labuwx@balfug.com>
2022-08-13 12:26:00 +0200

    LCWO transmit beep fix: Issue fixed on LCWO.net. https://git.fkurz.net/dj1yfk/lcwo/commit/9c7a7fac57df092b493b7d8f3ce8ff7e95c2430a

--- LCWO transmit beep fix/LCWO transmit beep fix.js
+++ LCWO transmit beep fix/LCWO transmit beep fix.js
@@ -2,21 +2,12 @@
 // @name         LCWO transmit beep fix
 // @description  Fix for the "An AudioContext was prevented from starting automatically" issue.
 // @namespace    https://greasyfork.org/en/users/697139-labuwx
-// @version      0.1
+// @version      1
 // @author       labuwx
 // @match        https://lcwo.net/transmit
 // @match        https://lcwo.net/*/transmit
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    var old_down_func = down;
-
-    down = (function() {
-        audioCtx.resume();
-        down = old_down_func;
-        down();
-    });
-})();
+// Issue fixed on LCWO.net. No need for a userscript.
+// https://git.fkurz.net/dj1yfk/lcwo/commit/9c7a7fac57df092b493b7d8f3ce8ff7e95c2430a
